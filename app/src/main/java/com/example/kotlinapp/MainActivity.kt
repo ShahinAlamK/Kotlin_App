1dd180ef1f19bdcdff730fd830c07fbd833ef2b6
SHAHIN <shahinalam23324@gmail.com>
2023-11-23 10:28:54 +0600

    widgets

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -1,40 +1,52 @@
 package com.example.kotlinapp
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    var randomNmber : Int = 0
+    private lateinit var snackBtn : Button
+    private lateinit var toastBtn : Button
+    private lateinit var dialogBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.statusBarColor = ContextCompat.getColor(this, R.color.black)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button : Button = findViewById(R.id.btn)
-        val snack : TextView = findViewById(R.id.textId)
+        snackBtn = findViewById(R.id.snack_id)
+        toastBtn = findViewById(R.id.toast_id)
+        dialogBtn = findViewById(R.id.dialog_Id)
 
-        snack.setOnClickListener{
-            Snackbar.make(it,"Hello SnackBar",Snackbar.LENGTH_SHORT)
+        snackBtn.setOnClickListener {
+            Snackbar.make(it,"Hello Kotlin Developer",Snackbar.LENGTH_SHORT)
                 .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
                 .show()
         }
 
-        button.setOnClickListener {
-            Toast.makeText(this,"Hello Kotlin $randomNmber",Toast.LENGTH_SHORT).show()
-            textChange()
+        toastBtn.setOnClickListener {
+            Toast.makeText(this,"Hello Kotlin Developer",Toast.LENGTH_SHORT)
+                .show()
         }
-    }
 
-    private fun textChange(){
-        randomNmber= Random.nextInt(10)+1
-        val textView : TextView = findViewById(R.id.textId)
-        textView.text = "Hello Kotlin Developer $randomNmber"
-    }
+        dialogBtn.setOnClickListener {
+            val dialog=AlertDialog.Builder(this)
+            dialog.setTitle("Are you Developer")
+            dialog.setMessage("Yes i'm kotlin developer")
+            dialog.setPositiveButton("Yes"){exite, which->
+                Toast.makeText(this,"Hello Kotlin Developer",Toast.LENGTH_SHORT).show()}
+            dialog.setNegativeButton("Cancel"){exite, which->
+                Toast.makeText(this,"Hello Kotlin Developer",Toast.LENGTH_SHORT).show()}
 
+            val builDialog : AlertDialog= dialog.create()
+            builDialog.show()
+        }
+
+
+    }
 }
